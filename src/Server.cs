d1025a25d3ca7ba19278bfbef8d24a7099440e2f
SrPatto <ladrianrv@hotmail.com>
2024-07-04 13:04:40 -0600

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -8,5 +8,21 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+
+using TcpClient client = await server.AcceptTcpClientAsync();
+await using NetworkStream stream = client.GetStream();
+
+byte[] requestBuffer = new byte[1024];
+int bytesRead = await stream.ReadAsync(requestBuffer, 0, requestBuffer.Length);
+string request = 
+    System.Text.Encoding.UTF8.GetString(requestBuffer, 0, bytesRead);
+Console.WriteLine($"Received request: {request}");
+
+var requestLines = request.Split("\r\n");
+var requests = requestLines[0].Split("/");
+
+// Prepare and send response
+string response = requests[1].Equals(" HTTP" ) ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+
+byte[] responseBuffer = System.Text.Encoding.UTF8.GetBytes(response);
+stream.Write(responseBuffer, 0, responseBuffer.Length);
